ddf8603344a34003bddd1e33fac1b9504d0a525f
DenisFefelov <fefelov_denis@mail.ru>
2013-01-19 08:52:27 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWiki::Application.config.secret_token = '4c21490dd733f9eb9765543a3aaccecfcf9d79093493add0422ce70cdb5b10d88f0e0ae24ba739eae1dcea7f3793114073bd024c7d2e591917a0667a959ef46c'
